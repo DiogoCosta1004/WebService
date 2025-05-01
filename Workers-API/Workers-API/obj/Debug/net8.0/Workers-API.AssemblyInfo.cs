4e0d7ef0ddfb9cb1bda37038aafb893bf934da38
DiogoCosta1004 <121439395+DiogoCosta1004@users.noreply.github.com>
2025-05-01 01:19:57 -0300

    Creating a project Axias Future

--- Workers-API/Workers-API/obj/Debug/net8.0/Workers-API.AssemblyInfo.cs
+++ Workers-API/Workers-API/obj/Debug/net8.0/Workers-API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Workers-API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+123dcd3b20fec08a71e20028d68b6ef576c95fba")]
 [assembly: System.Reflection.AssemblyProductAttribute("Workers-API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Workers-API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
